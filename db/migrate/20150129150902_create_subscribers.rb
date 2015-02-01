b6caa317f251998023a6a00b6ca2bc879493cfd9
dingxijin <fatelovely1128@gmail.com>
2015-02-01 22:54:06 +0800

    BUG FIX: add timestamps to subscribers model

--- db/migrate/20150129150902_create_subscribers.rb
+++ db/migrate/20150129150902_create_subscribers.rb
@@ -3,7 +3,7 @@ class CreateSubscribers < ActiveRecord::Migration
     create_table :subscribers do |t|
       t.string :email
 
-      t.timestamp
+      t.timestamps null:false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,9 @@ ActiveRecord::Schema.define(version: 20150129150902) do
   end
 
   create_table "subscribers", force: :cascade do |t|
-    t.string "email"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
