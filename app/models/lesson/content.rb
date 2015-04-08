99f3e960bb1c37d62c37f07bb60c2208b9deaf9a
hymRedemption <he.redemption@gmail.com>
2015-04-08 15:52:21 +0800

    lesson xml parse

--- app/models/lesson/content.rb
+++ app/models/lesson/content.rb
@@ -1,34 +1,6 @@
 class Lesson::Content
 
-  EXERCISE_TITLE = "h2"
-
-  SPECIAL_BLOCK_TAG = {
-                        "exercise" => {"class" => "exercise" },
-                        "screenshot" => {"class" => "guide" },
-                        "tip" => {"class" => "tip" },
-                        "code" => {"class" => "code"}
-                     }
-  SPECIAL_INLINE_TAG = {
-                         "video" => { "info_needed" => ["src"], "tag" => "video","options" => {"class" => "video"} },
-                         "exercise-test" => { "info_needed" => ["name"], "tag" =>"a", "options" => {"class" => "exercise_text" } }
-                       }
-  def haha
-    f = File.open(Rails.root + "lesson.xml")
-    xml = Nokogiri::HTML(f)
-    f.close
-
-    document = xml.document
-    parse_normal_block(xml, document)
-    pre_parse_special_block_tag(xml, document)
-    parse_exercise_block(xml, document)
-    parse_screenshot_block(xml, document)
-    parse_tip_block(xml, document)
-    add_tail_section(xml, document)
-    add_head_style(xml, document)
-    f = File.new(Rails.root + "hahaha.html", "w+")
-    f.write(xml.to_xhtml)
-    f.close
-  end
+  attr_reader :course_name, :version, :lesson_name
 
   def initialize(course_name, version, lesson_name)
     @course_name = course_name
@@ -36,265 +8,121 @@ class Lesson::Content
     @lesson_name = lesson_name
   end
 
-  def repo_path
-    Rails.root + "xml_repo" + @course_name
-  end
-
-  def html_page
-    git = Git.open(repo_path)
-    file = @course_name + ".xml"
-    xml = Nokogiri::HTML(git.show(@version, file))
-
-    xml = xml.css("page[name=#{@lesson_name}]")[0]
-    document = xml.document
-    parse_normal_block(xml, document)
-    pre_parse_special_block_tag(xml, document)
-    parse_exercise_block(xml, document)
-    parse_screenshot_block(xml, document)
-    parse_tip_block(xml, document)
-    xml.to_xhtml
-  end
-
-
-  def parse_tip_block(xml, document)
-    xml.css("tip").each do |tip_node|
-      p = Nokogiri::XML::Node.new("p", document)
-      tip_node.child.children.each do |child_node|
-        p.add_child(child_node)
-      end
-      tip_node.child.add_child(p)
-      tip_node.replace(tip_node.child)
-    end
-  end
-
-  def parse_normal_block(xml, document)
-    normal_node = normal_div(document)
-    xml.children.each do |child|
-      unless child.name == "exercise" || child.name == "tip" || child.name == "h1"
-        normal_node.add_child(child.dup)
-        child.remove
+  def html
+    html_content = ""
+    xml = xml_content
+    puts xml
+    xml.children.each do |node|
+      if node.name != "exercise"
+        html_content << node.to_xhtml
       else
-        if normal_node.element_children.size > 0
-          child.add_previous_sibling(normal_node)
-          normal_node = normal_div(document)
-        end
-        if child.name == "h1"
-          normal_node.add_child(child.dup)
-          child.remove
-        end
+        html_content << exercise_html(node)
       end
     end
-    if normal_node.element_children.size > 0
-      xml.add_child(normal_node)
-    end
+    html_content
   end
 
-  def normal_div(document)
-    div = Nokogiri::XML::Node.new("div", document)
-    div["class"] = "normal"
-    div
-  end
-
-  def pre_parse_special_block_tag(xml, document)
-    SPECIAL_BLOCK_TAG.each do |tag, options|
-      xml.css(tag).each do |node|
-        new_node_tag = (tag == "code") ? "pre" : "div"
-        new_node = Nokogiri::XML::Node.new(new_node_tag, document)
-        options.each do |key, value|
-          new_node[key] = value
-        end
-        if tag == "code"
-          new_node.add_child(node.dup)
-          node.replace(new_node)
-        else
-          clone_nodeset_as_children(node.children, new_node)
-          node.add_child(new_node)
-        end
-      end
-    end
-  end
-
-  def clone_nodeset_as_children(nodeset, parent)
-    nodeset.each do |node|
-      parent.add_child(node.dup)
-      node.remove
-    end
-  end
-
-  def parse_exercise_block(xml, document)
-    parse_exercise_head(xml, document)
-    parse_exercise_step(xml, document)
-    xml.css("exercise").each do |exercise_node|
-      ol_node = exercise_ol_node(document)
-      exercise_steps = exercise_node.css("exercise-step").sort
-      first_exercise_step = exercise_steps[0]
-      first_exercise_step.add_previous_sibling(ol_node)
-
-      exercise_steps.each do |exercise_step_node|
-        ol_node.add_child(exercise_step_node.child.dup)
-        exercise_step_node.remove
-      end
-      exercise_node.replace(exercise_node.child)
-    end
-  end
-
-
-  def next_node(document)
-    p = Nokogiri::XML::Node.new("p", document)
-    p["class"] = "next"
-    i = Nokogiri::XML::Node.new("i", document)
-    i["class"] = "fa fa-angle-double-down"
-
-    p.add_child(i)
-    p
-  end
-
-  def parse_img_option(img, screenshot_node)
-      info = screenshot_node["src"].split(/\?/)
-      src = info[0]
-      if info.length >= 2
-        option_info = info[1]
-        options = option_info.split(/ +/)
-        options.each do |opt|
-          opt_single = opt.split(/=/)
-          img[opt_single[0]] = opt_single[1]
-        end
-      end
-      img["src"] = "/" + @course_name + "/" + @lesson_name + "/" + src
-  end
-
-  def parse_screenshot_block(xml, document)
-    parent_node = nil
-    xml.css("screenshot").each do |screenshot_node|
-      #add next ico if the screenshot more than one in a step
-      img = img_node(document)
-      parse_img_option(img, screenshot_node)
-      screenshot_node.child.prepend_child(img)
-      if parent_node == screenshot_node.parent.parent #first parent is content_div.   content_div's parent is li
-        screenshot_node.add_previous_sibling(next_node(document))
-      end
-      parent_node = screenshot_node.parent.parent
-      screenshot_node.replace(screenshot_node.child)
-    end
-  end
-
-
-
-  def img_node(document)
-    img = Nokogiri::XML::Node.new("img", document)
-  end
-
-  def add_content_div_with_screenshot(exercise_step_node, document)
-    child = exercise_step_node.child
-    next_child = nil
-    content_div = content_div_node(document)
-
-    while child
-      next_child = child.next_sibling
-      if child.name != "screenshot"
-        content_div.add_child(child.dup)
-        child.remove if next_child != nil
+  def exercise_html(exercise_node)
+    content = ""
+    first_step = true
+    ol_tag_end = false
+    exercise_node.children.each do |node|
+      if node.name != "exercise-step"
+        content << node.to_xhtml
       else
-        content_div.add_child(child.dup)
-        child.replace(content_div)
-        content_div = content_div_node(document)
-      end
-      unless next_child
-        if content_div.element_children.size > 0
-          child.replace(content_div)
+        if first_step
+          out = <<-THERE
+          <ol class="steps">
+          THERE
+          content << out
+          first_step = false
+        end
+        content << exercise_step_html(node)
+        if node.next_sibling == nil || node.next_sibling.name != "exercise-step"
+          if !ol_tag_end
+            content << "</ol>"
+            ol_tag_end = true
+          end
         end
       end
-      child = next_child
     end
+    output =  <<-THERE
+    <div class="exercise">
+    #{content}
+    </div>
+    THERE
   end
 
-  def add_content_div_without_screenshot(exercise_step_node, document)
-      content_div = content_div_node(document)
-      child = exercise_step_node.child
-      next_child = nil
-      pre_child = nil
-
-      while child
-        next_child = child.next_sibling
-        content_div.add_child(child.dup)
-        child.remove if next_child
-        pre_child = child
-        child = next_child
+  def exercise_step_html(exercise_step_node)
+    content = ""
+    new_content_div = true
+    more_than_one_screenshot = false
+    exercise_step_node.children.each do |node|
+      if node.name != "screenshot"
+        if new_content_div
+          out = <<-THERE
+           <div class="content">
+          THERE
+          content << out
+          new_content_div = false
+        end
+        content << node.to_xhtml
+      else
+        if new_content_div
+          out = <<-THERE
+            <div class="content">
+          THERE
+          content << out
+        end
+        content << screenshot_html(node, more_than_one_screenshot)
+        more_than_one_screenshot = true
+        content << "</div>"
+        new_content_div = true
       end
-      pre_child.replace(content_div) if pre_child
+      if node.next_sibling == nil && !new_content_div
+        content << "</div>"
+      end
+    end
+
+    output = <<-THERE
+    <li class="steps">
+    #{content}
+    </li>
+    THERE
   end
 
-  def add_content_div(exercise_step_node, document)
-    if exercise_step_node.css("screenshot").size > 0
-      add_content_div_with_screenshot(exercise_step_node, document)
+  def screenshot_html(screenshot_node, next_tag)
+    content = ""
+    screenshot_node.children.each do |node|
+      content << node.to_xhtml
+    end
+
+    if next_tag
+      next_p = next_arrow
     else
-      add_content_div_without_screenshot(exercise_step_node, document)
+      next_p = ""
     end
 
-
+    output = <<-THERE
+    <div class="guide">
+    #{next_p}
+    #{content}
+    </div>
+    THERE
   end
 
-  def parse_exercise_step(xml, document)
-    xml.css("exercise-step").each do |exercise_step_node|
-      if exercise_step_node.child.text?
-        h3 = exercise_step_h3_node(document)
-        h3.content = exercise_step_node.child.text.strip
-        exercise_step_node.child.replace(h3)
-      end
-      li_node = exercise_step_li_node(document)
-
-      add_content_div(exercise_step_node, document)
-
-      clone_nodeset_as_children(exercise_step_node.children, li_node)
-      exercise_step_node.add_child(li_node)
-    end
+  def next_arrow
+    output = <<-THERE
+        <p class='next'>
+          <i class="fa fa-angle-double-down"></i>
+        </p>
+    THERE
   end
 
-  def content_div_node(document)
-    div = Nokogiri::XML::Node.new("div", document)
-    div["class"] = "content"
-    div
-  end
-
-  def exercise_ol_node(document)
-    ol_node = Nokogiri::XML::Node.new("ol", document)
-    ol_node["class"] = "steps"
-    ol_node
-  end
-
-  def exercise_step_h3_node(document)
-    Nokogiri::XML::Node.new("h3", document)
-  end
-
-  def exercise_step_li_node(document)
-    li_node = Nokogiri::XML::Node.new("li", document)
-    li_node["class"] = "step"
-    li_node
-  end
-
-
-  def parse_exercise_head(xml, document)
-    xml.css("exercise").each do |exercise_block|
-      exercise_div = exercise_block.first_element_child
-      exercise_title = exercise_div.first_element_child
-      if exercise_title.name == EXERCISE_TITLE
-        exercise_title.add_child(exercise_ico_node(document))
-      end
-    end
-  end
-
-  def exercise_ico_node(document)
-    div = Nokogiri::XML::Node.new("div", document)
-    div["class"] = "indicator"
-
-    i = Nokogiri::XML::Node.new("i", document)
-    i["class"] = "fa fa-wrench"
-
-    text = Nokogiri::XML::Text.new("Exercise", document)
-
-    div.add_child(i)
-    div.add_child(text)
-
-    div
+  def xml_content
+    git = Git.open(Course::Utils::XML_REPO_DIR + course_name)
+    file = course_name + ".xml"
+    xml = Nokogiri::HTML(git.show(version, file))
+    xml = xml.css("page[name=#{lesson_name}]")[0]
   end
 end
