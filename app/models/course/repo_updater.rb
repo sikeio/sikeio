afff905359fa641327c76344f3daaebfe91222c7
hymRedemption <he.redemption@gmail.com>
2015-04-08 11:37:58 +0800

    admin add course add update repo

--- app/models/course/repo_updater.rb
+++ app/models/course/repo_updater.rb
@@ -1,13 +1,13 @@
 class Course::RepoUpdater
 
-  attr_reader :course
+  attr_reader :course, :repo_dir_path
 
-  def initialize(temp_course)
+  def initialize(temp_course, repo_dir)
     @course = temp_course
+    @repo_dir_path = repo_dir
   end
 
   def update_repo
-    puts repo_dir_path
     if repo_cloned?
       pull_clone
     else
@@ -17,10 +17,6 @@ class Course::RepoUpdater
 
   def repo_cloned?
     File.exist?(repo_dir_path)
-  end
-
-  def repo_dir_path
-    Rails.root + "repo" + course.name
   end
 
   def pull_clone
