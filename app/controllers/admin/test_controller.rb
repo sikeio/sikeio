db3eaedfe4630d119741c27894c61b9af50902db
Howard Yeh <howard@metacircus.com>
2015-04-13 14:09:23 +0800

    admin can send email

--- app/controllers/admin/test_controller.rb
+++ app/controllers/admin/test_controller.rb
@@ -1,5 +1,8 @@
 class Admin::TestController < Admin::ApplicationController
   def send_email
-
+    email = params[:email]
+    msg = params[:message]
+    TestMailer.hello(email,msg).deliver_now!
+    render text: "email sent"
   end
 end
