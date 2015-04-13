a42fc7c4d27156e7d42a8dfc8ca22110ee152bbf
Howard Yeh <howard@metacircus.com>
2015-04-13 16:17:58 +0800

    redis config for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,13 @@
-redis_server = '127.0.0.1' # redis服务器
-redis_port = 6379 # redis端口
-redis_db_num = 12 # redis 数据库序号
+redis_server = ENV["REDIS_HOST"] || raise("Must set REDIS_HOST")
+redis_port = ENV["REDIS_PORT"] || 6379
+redis_db_num = ENV["SIDEKIQ_REDIS_DB"] || raise("Must set SIDEKIQ_REDIS_DB")
+
+redis_url = "redis://#{redis_server}:#{redis_port}/#{redis_db_num}"
 
 Sidekiq.configure_server do |config|
-  p redis_server
-  config.redis = { url: "redis://#{redis_server}:#{redis_port}/#{redis_db_num}"}
+  config.redis = { url: redis_url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{redis_server}:#{redis_port}/#{redis_db_num}" }
+  config.redis = { url: redis_url }
 end
