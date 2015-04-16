7a5179a2f60781edeb99eec4bff0d1587c2fd3be
Howard Yeh <howard@metacircus.com>
2015-04-16 21:11:54 +0800

    fix bad config for mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
           api_key: ENV['MAILGUN_API_KEY'],
-          domain: ENV["DOMAIN_NAME"]
+          domain: ENV["RAILS_HOST"]
   }
 end
