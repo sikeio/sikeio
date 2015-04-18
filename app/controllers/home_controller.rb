92fe0320edc73ab6a9b8fd739d3fba5f32702eb4
CJ Ting <dingxijin@CJ-MBP.local>
2015-04-18 20:53:30 +0800

    Add message box to index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
   def index
+    flash[:error] = '用户名或者密码错误！'
     @ios_course = Course.find_by(name: "ios")
     @nodejs_course = Course.find_by(name: "nodejs")
   end
