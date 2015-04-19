9d739316df7250451ddde214fbe8e7aa5ba2d27f
CJ Ting <dingxijin@CJ-MBP.local>
2015-04-19 11:40:03 +0800

    remove test flash error message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    flash[:error] = '用户名或者密码错误！'
     @ios_course = Course.find_by(name: "ios")
     @nodejs_course = Course.find_by(name: "nodejs")
   end
