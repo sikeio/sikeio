6dfad09397db813b09a60132cb34baf0dbf39d22
hymRedemption <he.redemption@gmail.com>
2015-04-22 09:14:29 +0800

    refactor file reader

--- app/models/course.rb
+++ app/models/course.rb
@@ -16,7 +16,7 @@
 
 class Course < ActiveRecord::Base
 
-  attr_reader :content_version
+  attr_reader :content_version, :xml_file_path, :asset_dir, :temp_dir, :repo_dir
 
   has_many :enrollments, dependent: :restrict_with_exception
 
@@ -28,6 +28,19 @@ class Course < ActiveRecord::Base
     format: {with: /\A[a-zA-Z0-9\-_]+\z/},
     uniqueness: true
   validates_uniqueness_of :permalink
+
+  after_initialize do |course|
+    xml_file = course.name + ".xml"
+    course.current_version = "master" if course.current_version.blank?
+    @xml_file_path = Course::Utils::XML_REPO_DIR + course.name + course.current_version + xml_file
+    @asset_dir = Course::Utils::ASSET_DIR + "courses"
+    @temp_dir = Course::Utils::TEMP_DIR
+    @repo_dir = Course::Utils::REPO_DIR + course.name
+    xml_dir = Course::Utils::XML_REPO_DIR + course.name + course.current_version
+    FileUtils.mkdir_p(xml_dir) if !File.exist?(xml_dir)
+    FileUtils.mkdir_p(@asset_dir) if !File.exist?(@asset_dir)
+    FileUtils.mkdir_p(@temp_dir) if !File.exist?(@temp_dir)
+  end
 
   def self.by_param(id)
     self.find_by(permalink: id)
