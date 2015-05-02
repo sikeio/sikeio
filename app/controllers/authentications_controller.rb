4da7d2f34217a21738a608f008792960dfc994bf
Howard Yeh <howard@metacircus.com>
2015-05-02 11:22:48 +0800

    event logging for oauth

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,23 +1,33 @@
 class AuthenticationsController < ApplicationController
   protect_from_forgery except: :callback
 
+  before_action only: [:new] do
+    log_event("oauth.init")
+  end
+
   def callback
-    logger.info({
-      event: "oauth.success",
-      payload: payload,
-      params: auth_params
-    }.to_json)
+    log_event("oauth.success",auth_attributes)
 
     auth = find_or_create_authentication
     login_as auth.user
+
     redirect_to_back_or_default auth_params["back_path"]
   end
 
   def fail
+    log_event("oauth.fail")
     redirect_to request.env['omniauth.origin'] || :root
   end
 
   private
+
+  def auth_attributes
+    {
+      provider: payload["provider"],
+      uid: payload["uid"].to_s,
+      info: payload
+    }
+  end
 
   def payload
     request.env["omniauth.auth"]
@@ -39,7 +49,7 @@ class AuthenticationsController < ApplicationController
     user = User.find_by(email: auth_email)
 
     if user.nil?
-      user = User.create! name: info['name'], email: auth_email
+      user = User.create! name: info['name'] || info['nickname'], email: auth_email
     end
 
     return user
@@ -48,12 +58,13 @@ class AuthenticationsController < ApplicationController
   def find_or_create_authentication
     auth = Authentication.find_by uid: payload["uid"]
     if auth
+      auth.update_attributes(info: payload)
       auth.user.update_attributes(email: auth_email)
       return auth
     end
 
     user = find_or_create_user
-    user.authentications.create!(payload)
+    user.authentications.create!(auth_attributes)
   end
 
 end
