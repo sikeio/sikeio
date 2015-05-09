fd33308657fbf0ebca9457f77941ada605315248
CJ Ting <dingxijin@CJ-MBP.local>
2015-05-09 20:51:24 +0800

    Use cookie to rememeber user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,17 +1,20 @@
 module SessionsHelper
 
   def current_user
-    @current_user ||= login_with_session
+    @current_user ||= login_with_session_or_cookies
   end
 
-  def login_with_session
-    if session[:user_id]
-      User.find_by id: session[:user_id]
+  def login_with_session_or_cookies
+    if id = session[:user_id]
+      User.find_by id: id
+    elsif id = cookies.signed[:user_id]
+      User.find_by id: id
     end
   end
 
   def login_as(user)
     session[:user_id] = user.id
+    cookies.permanent.signed[:user_id] = user.id
   end
 
   def login?
