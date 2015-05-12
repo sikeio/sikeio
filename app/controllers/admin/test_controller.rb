da69f70953f3b5a6626da25156d57e74bcace7ca
Howard Yeh <howard@metacircus.com>
2015-05-12 14:26:33 +0800

    print email send error

--- app/controllers/admin/test_controller.rb
+++ app/controllers/admin/test_controller.rb
@@ -3,7 +3,10 @@ class Admin::TestController < Admin::ApplicationController
     email = params[:email]
     msg = params[:message]
     TestMailer.hello(email,msg).deliver_now!
+
     render text: "email sent"
+  rescue RestClient::BadRequest => e
+    render text: "send error:\n#{e.response.body.force_encoding("utf-8")}"
   end
 
   def show_flash
