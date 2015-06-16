8d427c855a1b3852100aab858f3c8a1f53372c87
hymRedemption <he.redemption@gmail.com>
2015-06-16 10:46:37 +0800

    manual require mixpanel

--- app/jobs/mixpanel_track_job.rb
+++ app/jobs/mixpanel_track_job.rb
@@ -1,8 +1,10 @@
+require 'mixpanel-ruby'
+
 class MixpanelTrackJob < ActiveJob::Base
   queue_as :default
 
   def perform(id, event, properties)
-    tracker = ::Mixpanel::Tracker.new(ENV["MIXPANEL_TOKEN"])
+    tracker = Mixpanel::Tracker.new(ENV["MIXPANEL_TOKEN"])
     tracker.track(id, event, properties)
   end
 end
