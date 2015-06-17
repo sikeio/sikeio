1aa04820a92754dc45e4c1558b2d87160b772804
hymRedemption <he.redemption@gmail.com>
2015-06-17 21:44:44 +0800

    a probably solution

--- app/controllers/qiniu_controller.rb
+++ app/controllers/qiniu_controller.rb
@@ -4,5 +4,6 @@ class QiniuController < ApplicationController
     put_policy = Qiniu::Auth::PutPolicy.new(ENV['QINIU_BUCKET'])
     put_policy.save_key = "$(etag)$(ext)"
     @uptoken = Qiniu::Auth.generate_uptoken(put_policy)
+    render "uptoken.json.jbuilder"
   end
 end
