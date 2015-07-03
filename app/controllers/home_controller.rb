ba4f554202d508872cb94260e273db8f20fb7df4
hymRedemption <he.redemption@gmail.com>
2015-07-03 09:12:05 +0800

    home page layout css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,10 @@ class HomeController < ApplicationController
   def index
     @ios_course = Course.find_by(name: "ios")
     @nodejs_course = Course.find_by(name: "nodejs")
+    has_info = %w[nodejs design css ios].any? {|c| c == params[:course]}
+    @course_show = has_info ? params[:course] : "home"
+
+
 
     mixpanel_register(share: params[:share] || "direct")
     mixpanel_track(cookies.signed[:distinct_id], "Visited Home Page")
